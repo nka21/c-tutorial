6f94f4b30bcc3a04896e27cd644fdc64d043897b
nka21 <naoki.nico@gmail.com>
2024-09-11 18:36:55 +0900

    #3 コメントとエスケープシーケンス

---
+++ src/3/comments.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    /*
+        ターミナルに「Hello, world!」と出力される。
+        C言語の学習を始めたばかり。これは複数行コメント
+    */
+    printf("Hello, world!"); // 「Hello, world!」と出力される
+    return 0;
+}
---
+++ src/3/escape_sequences.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+
+    /*
+        \t -> tab
+        \n -> new line
+    */
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9");
+    return 0;
+}

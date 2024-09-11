33f0556d61bbda7557985b01be43cd0b533c25e6
nka21 <naoki.nico@gmail.com>
2024-09-11 20:41:21 +0900

    #3 ""に""を内包する方法

--- src/03_comments_escape_sequences/escape_sequences.c
+++ src/03_comments_escape_sequences/escape_sequences.c
@@ -4,7 +4,15 @@ int main() {
     /*
         \t -> tab
         \n -> new line
+
+        以下のコードで出力されるもの
+        1	2	3
+        4	5	6
+        7	8	9
     */
-    printf("1\t2\t3\n4\t5\t6\n7\t8\t9");
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9\n");
+
+    printf("\"I like Pizza.\" - Abraham Lincoln probably"); // \"で、""の中に、""を含むことができる。
+
     return 0;
 }
